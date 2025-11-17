25dc349fd495f1bc51fd255b767a4704f4a0ddc4
kanobayashi <kanobayashi209@gmail.com>
2025-11-17 16:26:39 +0900

    20251117

--- c_version/model.c
+++ c_version/model.c
@@ -28,8 +28,8 @@ void init_model(HumanSimulationModel* model, int num_agents, double width, doubl
 // --- 障害物設定部分を以下のように書き換える ---
     model->num_obstacles = 2;
     model->obstacles = (Obstacle*)malloc(sizeof(Obstacle) * model->num_obstacles);
-    model->obstacles[0] = (Obstacle){OBSTACLE_CIRCLE, {18.2, 0.8}, 0.4, 0.0, 0.0, {0.0,0.0}, {0.0,0.0}, 0.0};
-    model->obstacles[1] = (Obstacle){OBSTACLE_CIRCLE, {19.8, 0.8}, 0.4, 0.0, 0.0, {0.0,0.0}, {0.0,0.0}, 0.0};
+    model->obstacles[0] = (Obstacle){OBSTACLE_CIRCLE, {18.2, 0.8}, 0.4514, 0.0, 0.0, {0.0,0.0}, {0.0,0.0}, 0.0};
+    model->obstacles[1] = (Obstacle){OBSTACLE_CIRCLE, {19.8, 0.8}, 0.4514, 0.0, 0.0, {0.0,0.0}, {0.0,0.0}, 0.0};
 
     
     // model->obstacles[0] = (Obstacle){
